f3af6902c5fd750d7e0910dcd92638c7472a82a9
Prathamverma131 <85699196+Prathamverma131@users.noreply.github.com>
2022-09-27 21:14:50 +0530

    update

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-const a = 'Hell';
+const a = 'Hel';
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
